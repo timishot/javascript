bb8039e4df9d99fdf4d676a83ae8cb99cba55751
Timishot <timidotcom58@gmail.com>
2024-01-15 23:03:27 +0100

    Fix

--- ajax/103-script.js
+++ ajax/103-script.js
@@ -1,4 +1,5 @@
 $('document').ready(function () {
+	// it runs if only the webbrower is loaded
 	$('INPUT#btn_translate').click(translate);
 	$('INPUT#language_code').focus(function () {
 	  $(this).keydown(function (e) {
